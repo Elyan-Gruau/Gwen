d808aa7d0665e7bcb326cf77f95b104f87f8feba
Elyan <104207830+Elyan-Gruau@users.noreply.github.com>
2026-03-06 18:53:25 +0100

    Fix eslint rules

--- gwen-client/eslint.config.js
+++ gwen-client/eslint.config.js
@@ -3,23 +3,32 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import {defineConfig, globalIgnores} from 'eslint/config'
 import prettierConfig from 'eslint-config-prettier'
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-      prettierConfig,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    globalIgnores(['dist']),
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite,
+            prettierConfig,
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        rules: {
+            'no-restricted-syntax': [
+                'error',
+                {
+                    selector: 'ImportDeclaration[source.value=/\\.(ts|tsx|js)$/]',
+                    message: 'Do not include file extensions (.ts, .tsx, .js) in imports.',
+                },
+            ],
+        },
     },
-  },
 ])
