1eb726967cd5e88debb3db7638d1457629e88df0
Elyan <104207830+Elyan-Gruau@users.noreply.github.com>
2026-03-07 14:49:21 +0100

    Replace master with muster

--- gwen-common/src/types/CharacterCardAbilityType.ts
+++ gwen-common/src/types/CharacterCardAbilityType.ts
@@ -2,7 +2,7 @@ export type CharacterCardAbilityType =
   | 'AGILE' // Can be placed on the melee or distance row
   | 'MEDIC' // Resurrects a card from the discard pile
   | 'MORAL_BOOST' // Gain +1 power for each card in the same row (excluding itself)
-  | 'MASTER' // Place all the cards with the same name from your deck onto the battlefield
+  | 'MUSTER' // Place all the cards with the same name from your deck onto the battlefield
   | 'SPY' // Placed in the opponent's row, draws 2 cards from the deck
   | 'TIGHT_BOND' // Multiplies the power of all cards with the same name in the same row (2,3,4, etc...)
   | 'ROW_BOOST'
