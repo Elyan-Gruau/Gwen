86a3d0865467b04f629bb722fad80e87b5b0ca91
Elyan <104207830+Elyan-Gruau@users.noreply.github.com>
2026-03-07 12:45:52 +0100

    Improve game object

--- gwen-common/src/model/Game.ts
+++ gwen-common/src/model/Game.ts
@@ -1,17 +1,28 @@
-import {PlayerRows} from "./PlayerRows";
+import { PlayerRows } from './PlayerRows';
+import { Player } from './Player';
 
 export class Game {
+  private phase: GamePhase;
+  private players: Player[];
+  private playerRows: PlayerRows[];
 
-    private playerRows: PlayerRows[]
+  constructor() {
+    this.phase = 'WAITING_FOR_PLAYERS';
+    this.players = [new Player('player1'), new Player('player2')];
+    this.playerRows = this.players.map((player) => new PlayerRows(player.getUserId()));
+  }
 
-    constructor() {
-        this.playerRows = [
-            //TODO use the real userIds
-            new PlayerRows("player1"),
-            new PlayerRows("player2")
-        ]
+  getPlayers(): Player[] {
+    return this.players;
+  }
+
+  getPlayerRows(userId: string): PlayerRows {
+    const maybePlayerRows = this.playerRows.find((pr) => pr.getUserId() === userId);
+    if (!maybePlayerRows) {
+      throw new Error(`Player rows for player with id ${userId} not found`);
     }
-
+    return maybePlayerRows;
+  }
 }
 
-
+export type GamePhase = 'WAITING_FOR_PLAYERS' | 'REDRAW' | 'FLIP_COIN' | 'PLAY_CARDS' | 'END';
