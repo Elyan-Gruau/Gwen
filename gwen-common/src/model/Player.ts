579d71f45384aa65098ae98a1df922bcd86dbe04
Elyan <104207830+Elyan-Gruau@users.noreply.github.com>
2026-03-07 12:46:02 +0100

    Add deck to the player

--- gwen-common/src/model/Player.ts
+++ gwen-common/src/model/Player.ts
@@ -1,19 +1,30 @@
-import type {CharacterCard} from '../types/CharacterCard'
+import { Deck } from '../Deck';
+import { INITIAL_GEMS } from '../constants';
 
 export class Player {
-    private readonly id: string
-    private hands: CharacterCard[]
-    private deadCards: CharacterCard[]
-    private pioche: CharacterCard[]
+  private readonly userId: string;
+  private gems: number;
+  private deck: Deck;
 
-    constructor(id: string) {
-        this.id = id
-        this.hands = []
-        this.deadCards = []
-        this.pioche = []
-    }
+  constructor(id: string) {
+    this.userId = id;
+    this.deck = new Deck();
+    this.gems = INITIAL_GEMS;
+  }
 
-    getId(): string {
-        return this.id
-    }
+  getUserId(): string {
+    return this.userId;
+  }
+
+  hasGems(): boolean {
+    return this.gems > 0;
+  }
+
+  getGems(): number {
+    return this.gems;
+  }
+
+  getDeck(): Deck {
+    return this.deck;
+  }
 }
