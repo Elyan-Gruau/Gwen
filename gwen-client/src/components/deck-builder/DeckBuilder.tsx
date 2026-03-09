6ef0c87eac3e54d849b01a2bd545275af5c3c4b4
Elyan <104207830+Elyan-Gruau@users.noreply.github.com>
2026-03-09 18:29:23 +0100

    Add faction selector

--- gwen-client/src/components/deck-builder/DeckBuilder.tsx
+++ gwen-client/src/components/deck-builder/DeckBuilder.tsx
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import { Datapack, THE_WITCHER_DATAPACK } from 'gwen-common';
 import CardCollection from './card-collection/CardCollection';
 import FactionLeaderSelector from './FactionLeaderSelector';
+import FactionSelector from './FactionSelector';
 
 const DeckBuilder = () => {
-  const dumyDatapack = new Datapack(THE_WITCHER_DATAPACK);
-  const faction = dumyDatapack.getFactions()[0];
+  const datapack = new Datapack(THE_WITCHER_DATAPACK);
+  const factions = datapack.getFactions();
+
+  const [selectedFactionIndex, setSelectedFactionIndex] = useState(0);
+  const faction = factions[selectedFactionIndex];
+
   return (
     <div>
-      Faction selector
+      <FactionSelector
+        factions={factions}
+        selectedIndex={selectedFactionIndex}
+        onIndexChange={setSelectedFactionIndex}
+      />
       <div>
         Faction cards
         <CardCollection faction={faction} />
---
+++ gwen-client/src/components/deck-builder/FactionSelector.tsx
@@ -0,0 +1,33 @@
+import type { Faction } from 'gwen-common';
+import Button from '../reusable/button/Button';
+import styles from './FactionSelector.module.scss';
+
+type FactionSelectorProps = {
+  factions: Faction[];
+  selectedIndex: number;
+  onIndexChange: (index: number) => void;
+};
+
+const FactionSelector = ({ factions, selectedIndex, onIndexChange }: FactionSelectorProps) => {
+  const handlePrevious = () => {
+    onIndexChange((selectedIndex - 1 + factions.length) % factions.length);
+  };
+
+  const handleNext = () => {
+    onIndexChange((selectedIndex + 1) % factions.length);
+  };
+
+  return (
+    <div className={styles.factionSelector}>
+      <Button variant="ghost" size="sm" onClick={handlePrevious}>
+        ← Previous
+      </Button>
+      <span className={styles.factionName}>{factions[selectedIndex]?.getName()}</span>
+      <Button variant="ghost" size="sm" onClick={handleNext}>
+        Next →
+      </Button>
+    </div>
+  );
+};
+
+export default FactionSelector;
