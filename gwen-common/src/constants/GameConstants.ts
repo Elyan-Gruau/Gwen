0d5815e5bd5913fe9df5e931377be4bfec396018
Elyan <104207830+Elyan-Gruau@users.noreply.github.com>
2026-03-19 12:33:53 +0100

    Extract constants

---
+++ gwen-common/src/constants/GameConstants.ts
@@ -0,0 +1,18 @@
+/** Nombre de vies initiales par joueur */
+export const INITIAL_GEMS = 2;
+
+/** Nombre de manches pour gagner la partie (premier a perdre toutes ses vies) */
+export const TOTAL_ROUNDS = 3;
+
+/** Nombre de cartes en main au debut de la partie */
+export const INITIAL_HAND_SIZE = 10;
+
+/** Nombre de cartes echangeables en debut de partie (redraw) */
+export const MAX_REDRAW_COUNT = 2;
+
+/** Taille minimale d'un deck */
+export const MIN_DECK_SIZE = 22;
+
+// --- Scores ---
+
+/** Valeur d'une carte affectee par la meteo (non-heros) */
