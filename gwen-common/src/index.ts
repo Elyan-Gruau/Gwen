2a490cc8de73d8f4355260eee9a01da595e8eda5
Elyan <104207830+Elyan-Gruau@users.noreply.github.com>
2026-03-19 12:34:06 +0100

    Fix exports

--- gwen-common/src/index.ts
+++ gwen-common/src/index.ts
@@ -1,8 +1,12 @@
+export * from './model/Row';
+export * from './model/RowModifierCard';
 export * from './model/index.js';
 export * from './model/cards/UnitCard';
 export * from './types/RangeType.js';
 export * from './model/Row';
 export * from './model/RowModifierCard';
+
+// Export indexes
 export * from './constants/index.js';
 export * from './types/index.js';
 export * from './datapacks/index.js';
