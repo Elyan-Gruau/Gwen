1d2a7f705687f25aafd99cdf6078019d75130bce
Elyan <104207830+Elyan-Gruau@users.noreply.github.com>
2026-03-19 21:55:24 +0100

    make user profile pic fetch the user

--- gwen-client/src/components/user-profile-pic/UserProfilePic.tsx
+++ gwen-client/src/components/user-profile-pic/UserProfilePic.tsx
@@ -1,9 +1,27 @@
+import { useGetUser } from '../../hooks/apis/UserAPI';
+import Spinner from '../spinner/Spinner';
+
 export type UserProfilePicProps = {
   userId: string;
 };
 
 const UserProfilePic = ({ userId }: UserProfilePicProps) => {
-  return <div>UserProfilePic</div>;
+  const { data: user, isLoading, isError } = useGetUser(userId);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError || !user) {
+    return <div>Error loading user</div>;
+  }
+
+  const url = user.profilePictureUrl ?? ''; // TODO add default
+  return (
+    <div>
+      <img draggable={false} src={url} alt={`${user.username}-profile-picture`} />
+    </div>
+  );
 };
 
 export default UserProfilePic;
