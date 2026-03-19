0dbb8418c45c843c0b5ada5a9be5780ad08a6149
Elyan <104207830+Elyan-Gruau@users.noreply.github.com>
2026-03-19 12:33:41 +0100

    Connect to matchmaking pool

--- gwen-client/src/pages/matchmaking-page/MatchmakingPage.tsx
+++ gwen-client/src/pages/matchmaking-page/MatchmakingPage.tsx
@@ -1,4 +1,10 @@
+import { useMatchmaking } from '../../hooks/apis/MatckmakingAPI';
+import { useAuth } from '../../contexts/AuthContext';
+
 const MatchmakingPage = () => {
+  const { user } = useAuth();
+  useMatchmaking(user?._id!);
+
   return (
     <div>
       <h1>Matchmaking...</h1>
