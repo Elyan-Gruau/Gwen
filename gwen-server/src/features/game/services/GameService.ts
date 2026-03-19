2be2ea7741ff7a5e0a2968e1770c35ce070f8195
Elyan <104207830+Elyan-Gruau@users.noreply.github.com>
2026-03-19 12:39:56 +0100

    Create GameService.ts

---
+++ gwen-server/src/features/game/services/GameService.ts
@@ -0,0 +1,9 @@
+export class GameService {
+  public async createGame(player1Id: string, player2Id: string): Promise<{ _id: string }> {
+    // TODO: Save a game to the database
+    // TODO: Get the UserFactionDecks of the players
+    // TODO: Initialize the board
+    // TODO: Return game id
+    return { _id: 'temp-game-id' };
+  }
+}
