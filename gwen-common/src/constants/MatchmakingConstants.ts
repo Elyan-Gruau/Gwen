82e0372bef9aa9ee3c88568a2bbb9f446afa607d
Elyan <104207830+Elyan-Gruau@users.noreply.github.com>
2026-03-19 12:23:46 +0100

    Create MatchmakingConstants.ts

---
+++ gwen-common/src/constants/MatchmakingConstants.ts
@@ -0,0 +1,7 @@
+// Events CLIENT → SERVER
+export const MATCHMAKING_JOIN = 'matchmaking:join'; // The user joined the queue
+export const MATCHMAKING_LEAVE = 'matchmaking:leave'; // The user left the queue
+// Events SERVER → CLIENT
+export const MATCHMAKING_JOINED = 'matchmaking:joined'; // Joined confirmation
+export const MATCHMAKING_LEFT = 'matchmaking:left'; // Left confirmation
+export const MATCHMAKING_FOUND = 'matchmaking:found'; // Match found
