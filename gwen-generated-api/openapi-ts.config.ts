fa33b02b22c8403f033947eaa00b9214dc0cec86
Elyan <104207830+Elyan-Gruau@users.noreply.github.com>
2026-03-20 11:48:39 +0100

    Fix generation

--- gwen-generated-api/openapi-ts.config.ts
+++ gwen-generated-api/openapi-ts.config.ts
@@ -1,15 +1,10 @@
 // openapi-ts.config.ts
 import { defineConfig } from '@hey-api/openapi-ts';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  input: resolve(__dirname, './openapi.json'),
+  input: './openapi.json',
   output: {
-    path: resolve(__dirname, './src/generated'),
+    path: './src/generated',
     format: 'prettier',
   },
-  plugins: ['@hey-api/typescript', '@hey-api/services'],
 });
