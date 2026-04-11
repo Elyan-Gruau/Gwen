5aaf42f2a5e713bcef9a11855ec93f33fbc93da8
Elyan <104207830+Elyan-Gruau@users.noreply.github.com>
2026-04-11 15:07:35 +0200

    Add layout to the routes

--- gwen-client/src/router.tsx
+++ gwen-client/src/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Outlet } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import LoginPage from './pages/login-page/LoginPage';
 import LogoutPage from './pages/logout-page/LogoutPage';
 import SignInPage from './pages/sign-in-page/SignInPage';
@@ -10,58 +10,36 @@ import StatusPage from './pages/status-page/StatusPage';
 import DeckBuilder from './components/deck-builder/deck-builder/DeckBuilder';
 import ProfileMePage from './pages/profile-page/ProfileMePage';
 import ProtectedRoute from './components/auth/ProtectedRoute';
-import { useAuth } from './contexts/AuthContext';
+import NavbarLayout from './components/layouts/navbar-layout/NavbarLayout';
 import { ROUTES } from './constants/routes';
 
-const LoginRequiredPage = () => {
-  const { isInitialized, isAuthenticated } = useAuth();
-
-  if (!isInitialized) {
-    return <div>Loading...</div>;
-  }
-
-  if (isAuthenticated) {
-    return <Outlet />;
-  }
-
-  return (
-    <div>
-      <h1>Login required</h1>
-      <p>You must be logged in to play.</p>
-      <button type="button" onClick={() => (window.location.href = ROUTES.LOGIN)}>
-        Go to login
-      </button>
-    </div>
-  );
-};
-
 export const router = createBrowserRouter([
-  { path: ROUTES.HOME, element: <HomePage /> },
-  { path: ROUTES.HOME_ALT, element: <HomePage /> },
   { path: ROUTES.LOGIN, element: <LoginPage /> },
   { path: ROUTES.LOGOUT, element: <LogoutPage /> },
   { path: ROUTES.SIGN_IN, element: <SignInPage /> },
   { path: ROUTES.STATUS, element: <StatusPage /> },
   {
-    path: ROUTES.PROFILE,
-    element: (
-      <ProtectedRoute>
-        <ProfilePage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: ROUTES.PROFILE_ME,
-    element: (
-      <ProtectedRoute>
-        <ProfileMePage />
-      </ProtectedRoute>
-    ),
-  },
-  {
     path: '/',
-    element: <LoginRequiredPage />,
+    element: <NavbarLayout />,
     children: [
+      { path: ROUTES.HOME, element: <HomePage /> },
+      { path: ROUTES.HOME_ALT, element: <HomePage /> },
+      {
+        path: ROUTES.PROFILE,
+        element: (
+          <ProtectedRoute>
+            <ProfilePage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: ROUTES.PROFILE_ME,
+        element: (
+          <ProtectedRoute>
+            <ProfileMePage />
+          </ProtectedRoute>
+        ),
+      },
       {
         path: ROUTES.PLAY,
         element: <MatchmakingPage />,
