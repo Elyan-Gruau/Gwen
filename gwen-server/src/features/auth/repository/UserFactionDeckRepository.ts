cb5f79c928cad943df23218762a91eef8ece8041
Elyan <104207830+Elyan-Gruau@users.noreply.github.com>
2026-04-13 22:47:10 +0200

    Fix user faction deck update

--- gwen-server/src/features/auth/repository/UserFactionDeckRepository.ts
+++ gwen-server/src/features/auth/repository/UserFactionDeckRepository.ts
@@ -19,24 +19,25 @@ export class UserFactionDeckRepository {
     return savedDeck.toObject();
   }
 
-  async update(deck: DBUserFactionDeck): Promise<DBUserFactionDeck> {
-    const updated = await UserFactionDeckModel.findOneAndUpdate(
-      { user_id: deck.user_id, faction_id: deck.faction_id },
-      {
-        leader_card_id: deck.leader_card_id,
-        unit_card_ids: deck.unit_card_ids,
-        special_card_ids: deck.special_card_ids,
-        updated_at: new Date(),
-      },
-      { new: true },
-    )
-      .lean()
-      .exec();
-    if (!updated) {
-      throw new Error('Failed to update deck');
-    }
-    return updated;
-  }
+   async update(deck: DBUserFactionDeck): Promise<DBUserFactionDeck> {
+     const updated = await UserFactionDeckModel.findOneAndUpdate(
+       { user_id: deck.user_id, faction_id: deck.faction_id },
+       {
+         leader_card_id: deck.leader_card_id,
+         unit_card_ids: deck.unit_card_ids,
+         special_card_ids: deck.special_card_ids,
+         is_valid: deck.is_valid,
+         updated_at: new Date(),
+       },
+       { new: true },
+     )
+       .lean()
+       .exec();
+     if (!updated) {
+       throw new Error('Failed to update deck');
+     }
+     return updated;
+   }
 
   async delete(userId: string, factionId: string): Promise<boolean> {
     const result = await UserFactionDeckModel.deleteOne({
