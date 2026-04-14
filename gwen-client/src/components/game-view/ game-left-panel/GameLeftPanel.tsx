698f5dcd77324b68e8ea7f6b3e969488a5d02d7b
Elyan <104207830+Elyan-Gruau@users.noreply.github.com>
2026-04-15 01:13:31 +0200

    Improvae GameLeftPanel layout

--- gwen-client/src/components/game-view/ game-left-panel/GameLeftPanel.tsx
+++ gwen-client/src/components/game-view/ game-left-panel/GameLeftPanel.tsx
@@ -1,9 +1,11 @@
+import UserProfilePic from '../../user-profile-pic/UserProfilePic';
 import UserView from '../../user-view/UserView';
 import LeaderSlot from '../leader-slot/LeaderSlot';
 import styles from './GameLeftPanel.module.scss';
 import Gems from '../gem/Gems';
 import type { Player, PlayerRows } from 'gwen-common';
 import PlayerScore from '../player-score/PlayerScore';
+import { useGetUser } from 'gwen-generated-api';
 
 export type GameLeftPanelProps = {
   isCurrentPlayer: boolean;
@@ -12,13 +14,22 @@ export type GameLeftPanelProps = {
 };
 
 const GameLeftPanel = ({ isCurrentPlayer, player, playerRows }: GameLeftPanelProps) => {
+  const { data: user, isLoading } = useGetUser(player.getUserId());
+  const factionId = player.getDeck().getFactionId();
+
   return (
     <div className={styles.gameLeftPanel}>
-      <UserView userId={player.getUserId()} />
-      <Gems activeCount={player.getGems()} />
-      HandCount : {player.getDeck().getHand().length}
-      <PlayerScore playerRows={playerRows} />
-      <LeaderSlot />
+      <div className={styles.leftSection}>
+        <UserProfilePic userId={player.getUserId()} />
+      </div>
+      <div className={styles.rightSection}>
+        <div className={styles.infoStack}>
+          <div className={styles.handCount}>{player.getDeck().getHand().length}</div>
+          <Gems activeCount={player.getGems()} />
+        </div>
+        <div className={styles.username}>{user?.username ?? 'Unknown User'}</div>
+        <div className={styles.factionName}>{factionId}</div>
+      </div>
     </div>
   );
 };
