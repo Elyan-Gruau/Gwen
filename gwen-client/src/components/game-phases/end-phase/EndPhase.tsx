d0a63ffd0f2b27ea8be564b8a42ce25cc78fdd1e
Thewhyap <adrien.passeron06@gmail.com>
2026-04-14 16:14:30 +0200

    Light/dark mode fixed + icons

---
+++ gwen-client/src/components/game-phases/end-phase/EndPhase.tsx
@@ -0,0 +1,17 @@
+const EndPhase = () => {
+    /*const roundResults = {};
+    const winStatus = WIN_STATUS.WIN;
+
+
+
+
+    return (
+        <div>
+        <span>
+            {winStatus === WIN_STATUS.WIN ? 'You win!' : winStatus === WIN_STATUS.LOSS ? 'You lose!' : 'Draw!'}
+        </span>
+        </div>
+    );*/
+};
+
+export default EndPhase;
