b8364ba4d84d1baaf7d7ee28d40ae6abf9a93d94
Elyan <104207830+Elyan-Gruau@users.noreply.github.com>
2026-04-14 22:46:29 +0200

    Use empty deck slot in the discard/draw pile

--- gwen-client/src/components/game-view/game-right-panel/discarded-view/DiscardedPile.tsx
+++ gwen-client/src/components/game-view/game-right-panel/discarded-view/DiscardedPile.tsx
@@ -1,6 +1,8 @@
 import type { PlayableCard } from 'gwen-common';
 import CardListViewer from '../../../reusable/card-list-viewer/CardListViewer';
+import EmptyDeckSlot from '../../../reusable/empty-deck-slot/EmptyDeckSlot';
 import { useDisclosure } from '../../../../hooks/useDisclosure';
+import styles from './DiscardedPile.module.scss';
 
 export type DiscardedPileProps = {
   discarded: PlayableCard[];
@@ -8,10 +10,17 @@ export type DiscardedPileProps = {
 
 const DiscardedPile = ({ discarded }: DiscardedPileProps) => {
   const { isOpen, open, close } = useDisclosure(false);
+  const isEmpty = discarded.length === 0;
 
   return (
     <>
-      <div onClick={open}>discarded : {discarded.length}</div>
+      {isEmpty ? (
+        <EmptyDeckSlot title="Discarded pile is empty" />
+      ) : (
+        <div className={styles.container} onClick={open}>
+          discarded: {discarded.length}
+        </div>
+      )}
       <CardListViewer isOpen={isOpen} cards={discarded} onClose={close} title="Discard pile" />
     </>
   );
--- gwen-client/src/components/game-view/game-right-panel/draw-pile/DrawPile.tsx
+++ gwen-client/src/components/game-view/game-right-panel/draw-pile/DrawPile.tsx
@@ -1,6 +1,8 @@
 import type { PlayableCard } from 'gwen-common';
 import CardListViewer from '../../../reusable/card-list-viewer/CardListViewer';
+import EmptyDeckSlot from '../../../reusable/empty-deck-slot/EmptyDeckSlot';
 import { useDisclosure } from '../../../../hooks/useDisclosure';
+import styles from './DrawPile.module.scss';
 
 export type DeckpileProps = {
   drawPile: PlayableCard[];
@@ -8,10 +10,17 @@ export type DeckpileProps = {
 
 const DrawPile = ({ drawPile }: DeckpileProps) => {
   const { isOpen, open, close } = useDisclosure(false);
+  const isEmpty = drawPile.length === 0;
 
   return (
     <>
-      <div onClick={open}>drawpile: {drawPile.length}</div>
+      {isEmpty ? (
+        <EmptyDeckSlot title="Draw pile is empty" />
+      ) : (
+        <div className={styles.container} onClick={open}>
+          drawpile: {drawPile.length}
+        </div>
+      )}
       <CardListViewer isOpen={isOpen} cards={drawPile} onClose={close} title="Draw pile" />
     </>
   );
