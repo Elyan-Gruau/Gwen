411c2a08f8e04ebe2b8741edb239fb4a1efa77fd
Elyan <104207830+Elyan-Gruau@users.noreply.github.com>
2026-04-16 23:28:48 +0200

    Fix env variables

--- gwen-client/src/constants/api.ts
+++ gwen-client/src/constants/api.ts
@@ -4,8 +4,9 @@
 import { getEnvVariableWithFallback } from 'gwen-common';
 
 const DEFAULT_API_BASE_URL = 'http://localhost:3000/api';
-export const API_BASE_URL = import.meta.env.VITE_API_URL || DEFAULT_API_BASE_URL;
-if (import.meta.env.MODE === 'development' || import.meta.env.MODE === 'production') {
+const ENV_API_BASE_URL = import.meta.env.API_BASE_URL;
+export const API_BASE_URL = ENV_API_BASE_URL || DEFAULT_API_BASE_URL;
+if (!ENV_API_BASE_URL) {
   // eslint-disable-next-line no-console
-  console.debug('[ENV] API_BASE_URL utilisé :', API_BASE_URL);
+  console.debug('[ENV] API_BASE_URL found :', API_BASE_URL);
 }
