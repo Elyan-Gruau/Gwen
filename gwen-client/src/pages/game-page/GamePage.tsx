9c73d01a9250052cfe79d28abe0d2dfb01a9c90f
Elyan <104207830+Elyan-Gruau@users.noreply.github.com>
2026-04-16 15:23:21 +0200

    Clean game ui

--- gwen-client/src/pages/game-page/GamePage.tsx
+++ gwen-client/src/pages/game-page/GamePage.tsx
@@ -84,7 +84,6 @@ const GamePage = () => {
 
   return (
     <div className={styles.gamePage} style={style}>
-      <p>gameId : {gameId}</p>
       <GameView
         game={toModel(game)}
         gameMetadata={game}
